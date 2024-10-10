b94af37603383c814c154593d8a72f273e2240f9
adrsjr <farreladrian17@gmail.com>
2024-10-10 23:35:33 +0700

    UPDATE

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 // ini javascript
+
+console.log('javascript tersambung');
+
+function validateform() {
+    console.log('validasi fungsi berhasil terpanggil')
+    const Inputsuhu = document.getElementById('Input-suhu').value;
+    
+    if (Inputsuhu == ' ') {
+        alert('Inputan anda kosong');
+    } else {
+        calculate(Inputsuhu);
+    }
+    
+}
+
+function calculate(value) {
+    let result = parseInt(value) * 9 / 5 + 32
+    document.getElementById('Hasil-suhu').value = result;
+    document.getElementById('How-to-suhu').value = value + ' x 9 / 5 + 32 = ' + result;
+}
+
+function reset() {
+    document.getElementById('Input-suhu').value = ' ';
+    document.getElementById('Hasil-suhu').value = ' ';
+    document.getElementById('How-to-suhu').value = ' ';
+}
